caa33d8fbd9b83cec5defff311cea2027a2ca364
pastorcoder <zuma4good@gmail.com>
2022-12-29 01:58:25 +0100

    Done with <Text>

--- App.js
+++ App.js
@@ -1,31 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   console.log("App Executed");
 
 
-  let x = 1;
+  // let x = 1;
 
+
+  const handlePress = () => {
+    console.log("Key was pressed");
+  };
   
 
 
   return (
-    <View style={styles.container}>
-      <Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
         I need to get to the tutorial pretty quick and finish it before 31st
         decebmer 2022.
       </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "dodgerblue",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
